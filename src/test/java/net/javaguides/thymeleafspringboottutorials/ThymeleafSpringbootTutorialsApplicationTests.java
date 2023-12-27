9261a0fcf26f8bb6216e7f97257b84e3e24471a5
rania <chokrirania5544@gmail.com>
2023-12-27 12:47:36 +0100

    first commit

---
+++ src/test/java/net/javaguides/thymeleafspringboottutorials/ThymeleafSpringbootTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.thymeleafspringboottutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringbootTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
